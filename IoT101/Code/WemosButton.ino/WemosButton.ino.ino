139bb15edcae76fca3b71c2e7eac9207e7af2014
Coderkido <jgrrule@gmail.com>
2018-03-05 11:29:44 +1100

    Updated comments and added test mqtt server details

--- IoT101/Code/WemosButton.ino/WemosButton.ino.ino
+++ IoT101/Code/WemosButton.ino/WemosButton.ino.ino
@@ -1,84 +1,81 @@
+/*
+ Board: WeMos D1 MINI (https://tronixlabs.com.au/arduino/boards/wireless/wemos-d1-mini-arduino-compatible-esp8266-wifi-australia/)
+ Shield: https://tronixlabs.com.au/esp8266/wemos-esp8266/wemos-d1-mini-one-button-shield-australia/
+ Libraries: 
+  ESP8266Wifi (Add via Additional Boards Link): http://www.instructables.com/id/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
+  PubSubClient (Add via Library Manager): https://github.com/knolleary/pubsubclient
+
+ Whacked together by James Rule and Ethan Dunne for the MakerSpaceIoT 101 Workshop
+ */
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
+// Update these with values suitable for your network
+const char* ssid = "";
+const char* password = "";
+// The MQTT Server you want to use, this is https://test.mosquitto.org/ 
+const char* mqtt_server = "test.mosquitto.org"; 
 
-const char* ssid = "iAccelerate";
-const char* password = "AcceLer47eIC$";
-const char* mqtt_server = "13.210.157.158";
-
+// Variables for the MQTT Library
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-const int buttonPin = D3;
-const int ledPin = BUILTIN_LED;
-
-int buttonState = 0;
+const int buttonPin = D3; // For the WeMos D1 Mini Button Shield
+int buttonState = 0;  // Initialize the button state
+const int ledPin = BUILTIN_LED; 
 
 void setup() {
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-
-  // set initial state, LED off
-  digitalWrite(ledPin, buttonState);
+        Serial.begin(115200);
+        setup_wifi();
+        client.setServer(mqtt_server, 1883);        
+        pinMode(buttonPin, INPUT);
+        pinMode(ledPin, OUTPUT);  // set initial state, LED OFF
+        digitalWrite(ledPin, buttonState);  // LED Off
 }
 
 void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+        delay(10);
+        Serial.println();
+        Serial.print("Connecting to ");
+        Serial.println(ssid);        
+        WiFi.begin(ssid, password);        
+        while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        }        
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+        while (!client.connected()) {
+                Serial.print("Attempting MQTT connection...");
+                if (client.connect("ESP8266Client")) {
+                        Serial.println("connected");
+                } else {
+                        Serial.print("failed, rc=");
+                        Serial.print(client.state());
+                        Serial.println(" try again in 5 seconds");
+                        delay(5000);
+                }
+        }
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-
-  if (buttonState == LOW) {
-    reconnect();
-    Serial.println("Triggered!");
-    client.publish("/MSIOT101/IN", "B");
-    digitalWrite(ledPin, LOW);  // LED on
-    delay(1000);
-
-  } else {
-    digitalWrite(ledPin, HIGH); // LED off
-  }
+        buttonState = digitalRead(buttonPin);
+        if (buttonState == LOW) {
+                reconnect();
+                Serial.println("Triggered!");
+                client.publish("/MSIOT101/IN", "B");
+                digitalWrite(ledPin, LOW);  // LED ON
+                delay(1000);
+        } else {
+                digitalWrite(ledPin, HIGH); // LED OFF
+        }
 }
--- IoT101/Code/WemosRelay.ino/WemosRelay.ino.ino
+++ IoT101/Code/WemosRelay.ino/WemosRelay.ino.ino
@@ -1,18 +1,31 @@
+/*
+ Board: WeMos D1 MINI (https://tronixlabs.com.au/arduino/boards/wireless/wemos-d1-mini-arduino-compatible-esp8266-wifi-australia/)
+ Shield: https://tronixlabs.com.au/esp8266/wemos-esp8266/wemos-d1-mini-relay-shield-v2-australia/
+ Libraries: 
+  ESP8266Wifi (Add via Additional Boards Link): http://www.instructables.com/id/Wemos-ESP8266-Getting-Started-Guide-Wemos-101/
+  PubSubClient (Add via Library Manager): https://github.com/knolleary/pubsubclient
+
+ Whacked together by James Rule and Ethan Dunne for the MakerSpaceIoT 101 Workshop
+ */
+ 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "iAccelerate";
-const char* password = "AcceLer47eIC$";
-const char* mqtt_server = "13.210.157.158";
+// Update these with values suitable for your network
+const char* ssid = "";
+const char* password = "";
+// The MQTT Server you want to use, this is https://test.mosquitto.org/ 
+const char* mqtt_server = "test.mosquitto.org"; 
 
+// Variables for the MQTT Library
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-const int relayPin = D1;
-const long interval = 500; 
+const int relayPin = D1; // For the WeMos D1 Mini Relay Shield
+const long interval = 500; // The delay ensures the relay cannot be triggered too quickly 
 const int ledPin = BUILTIN_LED;
 
 void setup() {
@@ -20,11 +33,12 @@ void setup() {
         setup_wifi();
         client.setServer(mqtt_server, 1883);
         pinMode(relayPin, OUTPUT);
-        pinMode(ledPin, OUTPUT);
-        client.setCallback(callback);
+        pinMode(ledPin, OUTPUT);  // set initial state, LED OFF
+        digitalWrite(ledPin, HIGH); // LED Off
+        client.setCallback(callback); // The callback will trigger the relay when a message arrives
 }
 
-void setup_wifi() {
+void setup_wifi() { 
         delay(10);
         Serial.println();
         Serial.print("Connecting to ");
@@ -66,11 +80,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
         }
         Serial.println();        
         if ((char)payload[0] == '1') {        
-                digitalWrite(relayPin, HIGH); // turn on relay with voltage HIGH
+                digitalWrite(relayPin, HIGH); // Turn ON relay with voltage HIGH
                 digitalWrite(ledPin, LOW); // LED On
                 delay(interval);   
         } else {
-                digitalWrite(relayPin, LOW);  // turn off relay with voltage LOW
+                digitalWrite(relayPin, LOW);  // Turn OFF relay with voltage LOW
                 digitalWrite(ledPin, HIGH); // LED Off
                 delay(interval);
         }
